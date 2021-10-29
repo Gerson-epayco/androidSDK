4aaece4a88191496b91ea61b03d2e2400a299b1b
Gerson Vasquez <gerson.vasquez@epayco.com>
2021-10-29 13:06:15 -0400

    Se agrego consulta bancos

--- app/src/main/java/com/example/epaycosdk/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/epaycosdk/ui/home/HomeFragment.kt
@@ -8,8 +8,14 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import co.epayco.android.Epayco
+import co.epayco.android.models.Authentication
+import co.epayco.android.util.EpaycoCallback
 import com.example.epaycosdk.R
 import com.example.epaycosdk.databinding.FragmentHomeBinding
+import org.json.JSONException
+import org.json.JSONObject
+import java.lang.Exception
 
 class HomeFragment : Fragment() {
 
@@ -30,6 +36,34 @@ class HomeFragment : Fragment() {
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
+
+        val auth = Authentication()
+
+        auth.setApiKey("32c8ef12cc65878db1ccff30cdaf8e49")
+        auth.setPrivateKey("d84e9885d4f7de545e09736e9c5beb61")
+        auth.setLang("ES")
+        auth.setTest(true)
+
+
+        val epayco = Epayco(auth)
+
+        epayco.getBanks(object : EpaycoCallback {
+            @Throws(JSONException::class)
+            override fun onSuccess(data: JSONObject) {
+
+                System.out.println(data)
+                System.out.println("backs list")
+
+                val textViewBank: TextView = binding.textHome
+                textViewBank.text = data.toString();
+
+            }
+
+            override fun onError(error: Exception) {
+                System.out.println("Error")
+                System.out.println("Exception")
+            }
+        })
 
         val textView: TextView = binding.textHome
         homeViewModel.text.observe(viewLifecycleOwner, Observer {
